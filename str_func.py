913c6c9b82153cb0e40ebca0ffbf4c936687c354
Михаил Шикульский <shikul_m@mail.ru>
2023-03-31 18:22:42 +0400

    create function str_to_title

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def str_to_upper(str_inp: str) -> str:
     return str_inp.upper()
 
+
+def str_to_title(str_inp: str) -> str:
+    '''Делает заглавными первые буквы каждого слова в строке'''
+    return str_inp.title()
+
+
